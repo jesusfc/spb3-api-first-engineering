0e62040478c12a6c807352f0a9bdd3483cf2d97d
Jesusfc <jfcaraballo@gmail.com>
2024-05-07 12:31:08 +0200

    Start Maven project with Spring Boot 3 and Java 17

---
+++ src/main/java/com/jesusfc/apifirstengineering/ApiFirstEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.apifirstengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstEngineeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusfc/apifirstengineering/ApiFirstEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.apifirstengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
